c8fdffb84a1171e9decce0156471ad80f7c0d7f3
Benjamin Dehli <superelg@gmail.com>
2020-05-18 11:48:34 +0200

    Add helper methods for Brønnøysundregistrene API

---
+++ scripts/brregApiService.js
@@ -0,0 +1,92 @@
+const getOrganizationsByName = name => {
+  return fetch(`https://data.brreg.no/enhetsregisteret/api/enheter/?navn=${name}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    });
+}
+
+const getOrganizationsByOrganizationNumber = organizationNumber => {
+  return fetch(`https://data.brreg.no/enhetsregisteret/api/enheter/?organisasjonsnummer=${organizationNumber}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    });
+}
+
+const clearOrganizationSearchResultsDropdown = () => {
+  const containerElement = document.getElementById('organizationSearchResultsDropdown');
+  containerElement.classList.remove('hasContent');
+  containerElement.innerHTML = "";
+}
+
+const clearSelectedOrganization = () => {
+  document.getElementById('organizationName').value = "";
+  document.getElementById('organizationNumber').value = "";
+}
+
+const selectOrganization = organization => {
+  document.getElementById('organizationName').value = organization.navn;
+  document.getElementById('organizationSearch').value = organization.navn;
+  document.getElementById('organizationNumber').value = organization.organisasjonsnummer;
+  clearOrganizationSearchResultsDropdown();
+}
+
+
+const populateOrganizationSearchResultsDropdown = searchResults => {
+  const containerElement = document.getElementById('organizationSearchResultsDropdown');
+  containerElement.classList.add('hasContent');
+  containerElement.innerHTML = "";
+  searchResults.forEach(searchResult => {
+    let resultElement = document.createElement("DIV");
+    resultElement.setAttribute("class", "result");
+
+    let resultNameElement = document.createElement("SPAN");
+    resultNameElement.setAttribute("class", "resultName");
+    resultNameElement.innerHTML = searchResult.navn;
+
+    let resultOrganizationNumberElement = document.createElement("SPAN");
+    resultOrganizationNumberElement.setAttribute("class", "resultOrganizationNumber");
+    resultOrganizationNumberElement.innerHTML = searchResult.organisasjonsnummer;
+
+    resultElement.appendChild(resultNameElement);
+    resultElement.appendChild(resultOrganizationNumberElement);
+
+    resultElement.onclick = () => {
+      selectOrganization(searchResult)
+    };
+
+    containerElement.appendChild(resultElement);
+  })
+}
+
+const isFormattedAsOrganizationNumber = string => {
+  return !isNaN(string) && string.length === 9;
+}
+
+const searchResultsContainsItems = searchResults => {
+  return searchResults._embedded.enheter && searchResults._embedded && searchResults._embedded.enheter && searchResults._embedded.enheter.length;
+}
+
+const handleOrganizationSearchChange = searchString => {
+  clearSelectedOrganization();
+  if (searchString.length) {
+    isFormattedAsOrganizationNumber(searchString.replace(/\s/g, '')) ?
+      getOrganizationsByOrganizationNumber(searchString.replace(/\s/g, '')).then(searchResults => {
+        searchResultsContainsItems(searchResults) ? populateOrganizationSearchResultsDropdown(searchResults._embedded.enheter) : clearOrganizationSearchResultsDropdown();
+      }) :
+      getOrganizationsByName(searchString).then(searchResults => {
+        searchResultsContainsItems(searchResults) ? populateOrganizationSearchResultsDropdown(searchResults._embedded.enheter) : clearOrganizationSearchResultsDropdown();
+      });
+  } else {
+    clearOrganizationSearchResultsDropdown();
+  }
+}
+
+const handleOrganizationSearchBlur = () => {
+  setTimeout(clearOrganizationSearchResultsDropdown, 300);
+}
