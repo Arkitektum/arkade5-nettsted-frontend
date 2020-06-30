7465e6b8910484728d19b8d04702d9c42860f243
Benjamin Dehli <superelg@gmail.com>
2020-06-30 21:10:45 +0200

    Add post method for user info

---
+++ scripts/internalApiService.js
@@ -0,0 +1,20 @@
+const postUserInfo = userInfo => {
+  const apiUrl = 'http://arkade.arkitektum.no/api/arkade-downloads';
+  let filename = '';
+  try {
+    return fetch(apiUrl, {
+      method: 'POST',
+      body: JSON.stringify(userInfo),
+      headers: {
+       'Content-Type': 'application/json'
+     }
+   }).then(response => {
+    filename = response.headers.get('Filename');
+     response.blob()
+   }).then(blob => {
+      download(blob, filename);
+    });
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
