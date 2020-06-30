32964a64472c0c45ec8a81b1c0178fa5990acd47
Benjamin Dehli <superelg@gmail.com>
2020-06-30 21:38:40 +0200

    Fix issue with API blob promise

--- scripts/internalApiService.js
+++ scripts/internalApiService.js
@@ -1,19 +1,18 @@
 const postUserInfo = userInfo => {
   const apiUrl = 'https://arkade.arkitektum.no/api/arkade-downloads';
-  let filename = '';
   try {
     return fetch(apiUrl, {
       method: 'POST',
       body: JSON.stringify(userInfo),
       headers: {
-       'Content-Type': 'application/json'
-     }
-   }).then(response => {
-    filename = response.headers.get('Filename');
-     response.blob()
-   }).then(blob => {
-      download(blob, filename);
-    });
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      const filename = response.headers.get('Filename');
+      response.blob().then(blob => {
+        download(blob, filename);
+      });
+    })
   } catch (error) {
     console.error('Error:', error);
   }
